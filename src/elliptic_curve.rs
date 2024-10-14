0edf3e698d3f3df10ab494912ebd66c0512b6302
milosdjurica <milosdjurica13@gmail.com>
2024-10-15 00:30:34 +0200

    scalar_division and calculate_inverse and refactoring

--- src/elliptic_curve.rs
+++ src/elliptic_curve.rs
@@ -28,6 +28,31 @@ impl EllipticCurve {
         self.add_points(point1, &self.negate_point(point2))
     }
 
+    pub fn scalar_division(&self, scalar: BigUint, point: &Point) -> Option<Point> {
+        if scalar == BigUint::ZERO {
+            return None;
+        }
+
+        let inverse = self.calculate_inverse(&scalar);
+        Some(self.scalar_multiplication(&inverse, point))
+    }
+
+    pub fn scalar_multiplication(&self, scalar: &BigUint, point: &Point) -> Point {
+        let mut result = Point::new(None, None);
+        let mut current = point.clone();
+        let mut k = scalar.clone();
+
+        while k > BigUint::from(0u32) {
+            if &k % 2u32 == BigUint::from(1u32) {
+                result = self.add_points(&result, &current);
+            }
+            current = self.add_points(&current, &current);
+            k >>= 1;
+        }
+
+        result
+    }
+
     pub fn add_points(&self, point1: &Point, point2: &Point) -> Point {
         if point1.is_infinity() {
             return point2.clone();
@@ -48,35 +73,23 @@ impl EllipticCurve {
         Point::new(Some(x3), Some(y3))
     }
 
-    pub fn scalar_multiplication(&self, scalar: &BigUint, point: &Point) -> Point {
-        let mut result = Point::new(None, None);
-        let mut current = point.clone();
-        let mut k = scalar.clone();
-
-        while k > BigUint::from(0u32) {
-            if &k % 2u32 == BigUint::from(1u32) {
-                result = self.add_points(&result, &current);
-            }
-            current = self.add_points(&current, &current);
-            k >>= 1;
-        }
-
-        result
-    }
-
     fn calculate_slope(&self, x1: &BigUint, y1: &BigUint, x2: &BigUint, y2: &BigUint) -> BigUint {
         let numerator;
         let denominator;
         if x1 == x2 && y1 == y2 {
             // Point doubling
             numerator = (BigUint::from(3u32) * x1 * x1 + &self.a) % &self.p;
-            denominator =
-                (BigUint::from(2u32) * y1).modpow(&(&self.p - BigUint::from(2u32)), &self.p);
+            denominator = self.calculate_inverse(&(BigUint::from(2u32) * y1));
         } else {
             // Point addition
             numerator = (y2 + &self.p - y1) % &self.p;
-            denominator = (x2 + &self.p - x1).modpow(&(&self.p - BigUint::from(2u32)), &self.p);
+            denominator = self.calculate_inverse(&(x2 + &self.p - x1));
+            // denominator = (x2 + &self.p - x1).modpow(&(&self.p - BigUint::from(2u32)), &self.p);
         }
         (numerator * denominator) % &self.p
+    }
+
+    fn calculate_inverse(&self, number_to_invert: &BigUint) -> BigUint {
+        number_to_invert.modpow(&(&self.p - BigUint::from(2u32)), &self.p)
     }
 }
